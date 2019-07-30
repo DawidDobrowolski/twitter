7bf98b55a5820aa6902ac780d89b5f2873600a8c
Dawid Dobrowolski <dawid.dobrowolski@domado.pl>
2019-07-30 22:56:54 +0200

    spring security initialization

---
+++ src/main/java/com/twitter/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package com.twitter.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource);
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/twitter").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and().formLogin().loginPage("/login")
+                .and().logout().logoutSuccessUrl("/").permitAll();
+    }
+
+
+
+}
