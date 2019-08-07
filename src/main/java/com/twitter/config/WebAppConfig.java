35b01373ee6311c9eaf6396715c6c6f0b069442d
DawidDobrowolski <dawid.dobrowolski@domado.pl>
2019-08-07 16:58:47 +0200

    css, bootstrap, awesome font added + home page style

--- src/main/java/com/twitter/config/WebAppConfig.java
+++ src/main/java/com/twitter/config/WebAppConfig.java
@@ -1,6 +1,7 @@
 package com.twitter.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -12,5 +13,11 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/403").setViewName("403");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
