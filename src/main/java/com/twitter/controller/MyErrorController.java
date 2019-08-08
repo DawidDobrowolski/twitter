201d380097ae0d187d156232eecbbdf755b56734
DawidDobrowolski <dawid.dobrowolski@domado.pl>
2019-08-08 22:52:46 +0200

    last changes + error page

---
+++ src/main/java/com/twitter/controller/MyErrorController.java
@@ -0,0 +1,28 @@
+package com.twitter.controller;
+
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class MyErrorController implements ErrorController {
+
+    private static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public String error() {
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return PATH;
+    }
+}
