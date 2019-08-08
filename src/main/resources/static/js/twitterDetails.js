3cbb6cb72f52d61431db2f3f66632b407b86139b
DawidDobrowolski <dawid.dobrowolski@domado.pl>
2019-08-08 16:09:09 +0200

    view for tweet add and details

---
+++ src/main/resources/static/js/twitterDetails.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    $('#characterLeft').text('60 characters left');
+    $('#message').keyup(function () {
+        var max = 60;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#characterLeft').text('You have reached the limit');
+            $('#characterLeft').addClass('red');
+            $('form :submit').attr("disabled", "disabled");       }
+        else {
+            var ch = max - len;
+            $('#characterLeft').text(ch + ' characters left');
+            $('#characterLeft').removeClass('red');
+            $('form :submit').removeAttr("disabled");
+        }
+    });
+
+    $('#deleteModal').on('show.bs.modal', function (event) {
+        var tweetId = $(event.relatedTarget).data('tweet-id');
+        var commentId = $(event.relatedTarget).data('comment-id');
+        $('#deleteId').on('click', function () {
+            window.location.href = "/comment/delete/" + tweetId + "/" + commentId;
+        })
+    });
+
+})
